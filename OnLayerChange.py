465a26532983586881870b207ef7312035160e90
pmarkiewicz <piotr.markiewicz@gmail.com>
2018-05-01 13:18:21 +0100

    Initial commit, on layer change

---
+++ OnLayerChange.py
@@ -0,0 +1,55 @@
+from ..Script import Script
+
+
+class OnLayerChange(Script):
+    def __init__(self):
+        super().__init__()
+
+    def getSettingDataString(self):
+        return """{
+            "name": "On layer change",
+            "key": "OnLayerChange",
+            "metadata": {},
+            "version": 2,
+            "settings":
+            {
+                "action":
+                {
+                    "label": "Action",
+                    "description": "Action on layer change. Separate by | to alternate layers. Use {} to insert layer no",
+                    "type": "str",
+                    "default_value": ""
+                }
+            }
+        }"""
+
+    def execute(self, data: list):
+        """data is a list. Each index contains a layer"""
+
+        action = self.getSettingValueByKey("action").split("|")
+        no_of_actions = len(action)
+
+        for index, layer in enumerate(data):
+            lines = layer.split("\n")
+            for line in lines:
+                if not line.startswith(";LAYER:"):
+                    continue
+
+                try:
+                    layer_no = int(line[len(";LAYER:"):])
+                except ValueError: #Couldn't cast to int. Something is wrong with this g-code data.
+                    continue
+
+                action_no = layer_no % no_of_actions
+                prepend_gcode = ";TYPE:CUSTOM\n"
+                prepend_gcode += ";added code by post processing\n"
+                prepend_gcode += ";script: OnLayerChange.py action no: {0}\n".format(action_no)
+                prepend_gcode += action[action_no].format(layer_no) + "\n"
+
+                layer = prepend_gcode + layer
+
+                # Override the data of this layer with the
+                # modified data
+                data[index] = layer
+
+        return data
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CuraPostprocessingScripts
-Additional post processing scripts for Cura
+
+Additional post processing scripts for Cura.
+
+Install
+In Cura goto Help -> Show configuration folder and copy scripts to scripts folder.
+
+OnLayerChange - actions for every layer, can be used to trigger camera to get better timelapse.
+
